a7b128c5858ab01c68ba5551cfe51c589c5fbdf6
MacGregg0r <43099079+MacGregg0r@users.noreply.github.com>
2026-02-13 11:50:26 -0700

    code to just clean data not explore it

--- 311_cleaner.py
+++ 311_cleaner.py
@@ -4,6 +4,24 @@ import pandas as pd
 # load data set into pandas dataframe
 df = pd.read_csv("311_Service_Requests.csv")
 
+# ALREADY EXPORTED SO NO NEEDED TO RUN
+# extract unique service names and export to a txt file
+# service_names = sorted(df["service_name"].dropna().unique())
+# with open("service_names.txt", "w") as file:
+#     for name in service_names:
+#         file.write(f"{name}\n")
+
+# ALREADY CHECKED SO NO NEEDED TO RUN
+# check different types of statuses
+# statuses = sorted(df["status_description"].dropna().unique())
+# print(statuses)
+
+# remove duplicate and to be deleted items
+removable_statuses = ["Duplicate (Closed)", "Duplicate (Open)", "TO BE DELETED"]
+print(len(df))
+df = df[~df["status_description"].isin(removable_statuses)]
+print(len(df))
+
 # trim unnecessary columns
 columns_to_drop = ["service_request_id", "source", "address", "comm_code",
                    "location_type", "longitude", "latitude", "point", "updated_date"]
@@ -30,7 +48,5 @@ for column in useful_service_names_df.columns:
         category[value] = column
 df["category"] = df["service_name"].map(category)
 
-
-
 # export csv from cleaned dataframe
-df.to_csv("311_clean_service_requests.csv")
+# df.to_csv("311_clean_service_requests.csv")
