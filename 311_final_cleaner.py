544354c056b84f6c81045042f549f89f557f4adc
MacGregg0r <43099079+MacGregg0r@users.noreply.github.com>
2026-02-16 19:39:25 -0700

    script to clean dataset
    please add or modify as needed so everyone can have the same dataset after running code.

---
+++ 311_final_cleaner.py
@@ -0,0 +1,28 @@
+# imports
+import pandas as pd
+
+# load data set into pandas dataframe
+df = pd.read_csv("311_Service_Requests.csv")
+
+# remove rows that contain "Inquiry" or "Request"
+df = df[~df["service_name"].str.contains("Inquiry", na=False)]
+df = df[~df["service_name"].str.contains("Request", na=False)]
+
+# remove duplicate and to be deleted items
+removable_statuses = ["Duplicate (Closed)", "Duplicate (Open)", "TO BE DELETED"]
+df = df[~df["status_description"].isin(removable_statuses)]
+
+# trim unnecessary columns
+columns_to_drop = ["address", "location_type", "point", "updated_date"]
+df.drop(columns_to_drop, axis=1, inplace=True)
+
+# convert dates to usable format and remove the time stamps
+df["requested_date"] = pd.to_datetime(df["requested_date"], format="%Y/%m/%d %I:%M:%S %p").dt.date
+df["closed_date"] = pd.to_datetime(df["closed_date"], format="%Y/%m/%d %I:%M:%S %p").dt.date
+
+# trim rows with blank community names, service name
+df = df[df["comm_name"].notna()]
+df = df[df["service_name"].notna()]
+
+# export csv from cleaned dataframe
+df.to_csv("311_clean_service_requests.csv", index=False)
