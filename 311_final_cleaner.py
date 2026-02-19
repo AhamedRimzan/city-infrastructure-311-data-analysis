184b1b58b3b7fb5696e3c7c9c49c929ab5db83c8
MacGregg0r <43099079+MacGregg0r@users.noreply.github.com>
2026-02-18 20:17:31 -0700

    FINAL PUSH displays and exports summary of data

--- 311_final_cleaner.py
+++ 311_final_cleaner.py
@@ -1,5 +1,6 @@
 # imports
 import pandas as pd
+pd.set_option('display.max_columns', None)
 import os
 import glob
 
@@ -27,10 +28,17 @@ elif len(possible_files) > 1:
             print("Please enter a valid number")
 else: print("No valid file found")
 
+# create a csv with a summary of the input dataset
+input_summary_df = pd.DataFrame({"column name": df.columns,
+                           "input unique values": df.nunique(dropna=True).values,
+                           "input null values": df.isna().sum().values,
+                           "input data type": df.dtypes.astype(str).values})
+
 # print info on dataset
 print("This dataset contains: ")
 print(f"{len(df.columns)} columns")
 print(f"{len(df)} rows")
+print(input_summary_df)
 
 # remove rows that contain "Inquiry" or "Request"
 print("Removing inquiries...")
@@ -74,17 +82,27 @@ df.loc[mask, "agency_responsible"] = "Unknown"
 print("Removing more blank data...")
 df = df[(df["status_description"] == "Open") | ~df.isna().any(axis=1)]
 
+# add to the summary csv with a summary of the output dataset
+output_summary_df = pd.DataFrame({"column name": df.columns,
+                           "output unique values": df.nunique(dropna=True).values,
+                           "output null values": df.isna().sum().values,
+                           "output data type": df.dtypes.astype(str).values})
+
 # print info for exported file
 print("This exported dataset contains: ")
 print(f"{len(df.columns)} columns")
 print(f"{len(df)} rows")
-print("Blanks remaining in each column: ")
-for column in df.columns:
-    blanks = (df[column].isna()).sum()
-    print(f"{column}: {blanks}")
+print(output_summary_df)
+
+# export a summary csv
+summary_df = pd.merge(input_summary_df, output_summary_df, on="column name", how="left")
+summary_df = summary_df[["column name", "input unique values", "output unique values", "input null values",
+                         "output null values", "input data type", "output data type"]]
+summary_df.to_csv(os.path.join(download_folder, "311_service_requests_summary.csv"), index=False)
 
 # export csv from cleaned dataframe
 print("Exporting cleansed CSV...")
 df.to_csv(os.path.join(download_folder, "311_clean_service_requests.csv"), index=False)
-print(f"Successful, your cleansed CSV has been exported to "
-      f"{os.path.join(download_folder, "311_clean_service_requests.csv")}")
+print(f"Successful, your cleansed CSV and summary CSV have been exported to \n "
+      f"{os.path.join(download_folder, "311_clean_service_requests.csv")} and \n"
+      f"{os.path.join(download_folder, "311_service_requests_summary.csv")}")
